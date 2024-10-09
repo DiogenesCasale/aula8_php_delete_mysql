8b0dcdc51cc7bf2d5211198bd9078b2a02e73804
DiogenesCasale <166877237+DiogenesCasale@users.noreply.github.com>
2024-10-08 21:48:19 -0300

    Add files via upload

---
+++ mostrafutebol.php
@@ -0,0 +1,28 @@
+<?php
+
+$servidor = 'localhost';
+$banco = 'futebol';
+$usuario = 'root';
+$senha = '';
+
+$conexao = new PDO("mysql:host=$servidor;dbname=$banco", $usuario, $senha);
+$comandoSQL = 'SELECT `id`, `nome`, `pontos` FROM `time`';
+$comando = $conexao->prepare($comandoSQL);
+$resultado = $comando->execute();
+
+if($resultado) {
+  echo "Mostrando resultado:<br><br>";
+while($linha = $comando->fetch()) {
+    echo "Nome do Time: " . $linha['nome'];
+    echo "<br>";
+    echo "Quantidade de Pontos: " . $linha['pontos'];
+    echo "<br>";
+    echo "<a href='apagatime.php?id={$linha['id']}'>Apagar</a><br><br>";
+  }
+} else {
+  echo "Erro no comando SQL";
+}  
+        
+$conexao = null;
+
+?>
